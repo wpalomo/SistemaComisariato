674205b5c9adbc5d5cfd55866998d667306c3f9d
andresCedeno26 <33564537+andresCedeno26@users.noreply.github.com>
2018-01-06 21:22:38 -0500

    andres

---
+++ Comisariato/Comisariato/Formularios/Transacciones/Venta/FrmConsultarPrecios.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace Comisariato.Formularios.Transacciones.Venta
+{
+    partial class FrmConsultarPrecios
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTotalPagar = new System.Windows.Forms.Label();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSalirCompra = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTotalPagar
+            // 
+            this.lblTotalPagar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblTotalPagar.BackColor = System.Drawing.Color.White;
+            this.lblTotalPagar.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblTotalPagar.Font = new System.Drawing.Font("Impact", 39.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotalPagar.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.lblTotalPagar.Location = new System.Drawing.Point(55, 36);
+            this.lblTotalPagar.Name = "lblTotalPagar";
+            this.lblTotalPagar.Size = new System.Drawing.Size(281, 77);
+            this.lblTotalPagar.TabIndex = 34;
+            this.lblTotalPagar.Text = "0.00";
+            this.lblTotalPagar.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCodigo.Location = new System.Drawing.Point(143, 131);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(151, 26);
+            this.txtCodigo.TabIndex = 36;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Arial", 10F);
+            this.label6.ForeColor = System.Drawing.Color.Teal;
+            this.label6.Location = new System.Drawing.Point(80, 137);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(57, 16);
+            this.label6.TabIndex = 35;
+            this.label6.Text = "Código:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Teal;
+            this.label1.Location = new System.Drawing.Point(115, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(162, 18);
+            this.label1.TabIndex = 37;
+            this.label1.Text = "Consulta de Producto";
+            // 
+            // btnSalirCompra
+            // 
+            this.btnSalirCompra.Image = global::Comisariato.Properties.Resources.salir2;
+            this.btnSalirCompra.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnSalirCompra.Location = new System.Drawing.Point(240, 196);
+            this.btnSalirCompra.Name = "btnSalirCompra";
+            this.btnSalirCompra.Size = new System.Drawing.Size(96, 53);
+            this.btnSalirCompra.TabIndex = 38;
+            this.btnSalirCompra.Text = "Salir";
+            this.btnSalirCompra.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnSalirCompra.UseVisualStyleBackColor = true;
+            // 
+            // FrmConsultarPrecios
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Bisque;
+            this.ClientSize = new System.Drawing.Size(396, 261);
+            this.Controls.Add(this.btnSalirCompra);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtCodigo);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.lblTotalPagar);
+            this.Name = "FrmConsultarPrecios";
+            this.Text = "FrmConsultarPrecios";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTotalPagar;
+        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSalirCompra;
+    }
+}
